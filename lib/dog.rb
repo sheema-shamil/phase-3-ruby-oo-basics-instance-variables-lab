e4184967fce7b2caae5ff98e703f6fcaa084da06
sheema <sheemashamilfarook@gmail.com>
2023-02-10 08:54:51 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+     # setter
+     def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #  getter
+    def name
+        @this_dogs_name
+    end
 end
+
+fino = Dog.new
+fino.name ="fino"
+puts fino.name
